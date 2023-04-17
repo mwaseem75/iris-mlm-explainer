355a17bf97bc614259eaa3ccf98038e22ee0aa67
mwaseem75 <mwaseem75@hotmail.com>
2023-04-17 10:45:22 +0300

    commit

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This web application connects to InterSystems Cloud SQL, display a dashboard of 
 The dashboard includes:
 - SHAP values (i.e. what is the contribution of each feature to each individual prediction?)
 - Permutation importances (how much does the model metric deteriorate when you shuffle a feature?)
-- Partial dependence plots (how does the model prediction change when you vary a single feature?
+- Partial dependence plots (how does the model prediction change when you vary a single feature?)
 - For Random Forests and xgboost models: visualization of individual trees in the ensemble.
 - Plus for classifiers: precision plots, confusion matrix, ROC AUC plot, PR AUC plot, etc
 - For regression models: goodness-of-fit plots, residual plots, etc.
--- expdash.py
+++ expdash.py
@@ -5,33 +5,21 @@ import dash_bootstrap_components as dbc
 from sklearn.preprocessing import OrdinalEncoder
 import utility
 
-
-#train_names, test_names = titanic_names()
-
-#ExplainerDashboard(ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)).run()
-#explainer = ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
-
-#explainer = RegressionExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
-
-#explst = [explainer]
-#explainer = ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
-#explst.append(explainer)
-#explst
-
-#explainer1 = ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
-#explainer2 = ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
-
-#db1 = ExplainerDashboard(ClassifierExplainer(, X_test, y_test))
-#db2 = ExplainerDashboard(ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test))
-
+#Establish connection with InterSystems IRIS Cloud
 connection = utility.get_db_connection()
+#Create cursor 
 cur = connection.cursor()
+#Execute the cursor to retrieve all the models
 cur.execute('SELECT MODEL_NAME,DESCRIPTION,DEFAULT_TRAINING_QUERY,PREDICTING_COLUMN_NAME FROM INFORMATION_SCHEMA.ML_MODELS')
+#Fetch all the records in data
 data = cur.fetchall()
 dblst=[]
+#Iterate all the records and check if model is trained
 for i in range(len(data)):
+    #check if model is trained
     cur.execute("select lower(max(model_type)) from information_schema.ML_TRAINED_MODELS where model_name = '"+data[i][0]+"'")
     data2 = cur.fetchall()
+    #if Model type is regression
     if data2[0][0] == 'regression':
         #explainer = RegressionExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
         #X_train, y_train, X_test, y_test = utility.get_model_train_test(data[i][2],data[i][3])
@@ -48,39 +36,37 @@ for i in range(len(data)):
         #X_test.to_csv("x_test.csv")
         #y_test.to_csv("y_test.csv")
         
+        #Get train and test data
         X_train, y_train, X_test, y_test = titanic_fare()
         #X_train._convert(convert_numeric=True).dropna()
         #X_train2 = X_train.drop(columns=['store_and_fwd_flag'])
         #encoder = OrdinalEncoder()
         #X_train = encoder.fit_transform(X_train)
         #X_test = encoder.fit_transform(X_test)
+        #Generate model
         model = RandomForestRegressor(n_estimators=50, max_depth=10).fit(X_train, y_train)
+        #Initiate explainer 
         explainer = RegressionExplainer(model, X_test, y_test)
-        dblst.append(ExplainerDashboard(explainer,title="Model : "+data[i][0], name="db"+ str(i+1),description=data[i][1]+", Training Query : "+data[i][2],bootstrap=dbc.themes.SLATE))
+        #Append exlpaner to the list, so can be added to ExplainerHub
+        dblst.append(ExplainerDashboard(explainer,title="Model : "+data[i][0], name="db"+ str(i+1),description=data[i][1]+", Training Query : "+data[i][2]))
+    #If Model type is classification
     elif data2[0][0] == 'classification':
         #X_train, y_train, X_test, y_test = utility.get_model_train_test(data[i][2],data[i][3])
+        #Get train and test data
         X_train, y_train, X_test, y_test = titanic_survive()
         encoder = OrdinalEncoder()
         X_train = encoder.fit_transform(X_train)
+        #Initiate explainer 
         explainer = ClassifierExplainer(RandomForestClassifier().fit(X_train, y_train), X_test, y_test)
+        #Append exlpaner to the list, so can be added to ExplainerHub
         dblst.append(ExplainerDashboard(explainer,title="Model : "+data[i][0], name="db"+ str(i+1),description=data[i][1]+", Training Query : "+data[i][2],bootstrap=dbc.themes.SLATE))
     else:
         pass
     
 #close connection
 cur.close() 
-#db1 = ExplainerDashboard(explainer1,title=data[0][0], name="db1",description="This is model option one")
-#db2 = ExplainerDashboard(explainer2,title=data[1][0], name="db2",description="This is model option two")
-
-#db1 = ExplainerDashboard(explainer1,title="d221", name="db1",description="This is model option one")
-#db2 = ExplainerDashboard(explainer2,title="d223", name="db2",description="This is model option two")
-
-
+connection.close()
+#initate ExplanerHub by passing all the explaners
 hub = ExplainerHub(dblst, bootstrap=dbc.themes.MORPH, title="InterSystems IRIS Cloud Integrated ML explorer",description="Give your applications direct access to the advanced relational database capabilities of InterSystems IRIS® Data Platform without the burden of provisioning, configuring, and maintaining cloud infrastructure. The IRIS Cloud IntegratedML option lets you define and execute predictive models by applying automated functions directly from SQL, without requiring extensive machine learning expertise.")
-#hub = ExplainerHub(dblst, no_index=True)
+#Run application
 hub.run()
-#app = hub.flask_server()
-
-#@app.route("/")
-#def custom_index():
-#    return render_template('index.html')
